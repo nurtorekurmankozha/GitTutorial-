04327bb87bd84aaebeeb29326eb4633c2d1884c7
Nurtore <180103146@stu.sdu.edu.kz>
2019-04-04 13:02:39 +0600

    changes Hello.java

--- Hello.java
+++ Hello.java
@@ -1,5 +1,5 @@
 public class Hello{
 	public static void main(string[] args){
-		System.out.println("Hello, my friends. How are you!");
+		System.out.println("Hello, my friends. How are you!!!");
 	}
 }
